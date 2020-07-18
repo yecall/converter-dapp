1f184167283ea110bf52bb5a9513aa1cbb9e12ac
xmg <xiaomenggang520600@163.com>
2020-07-18 19:37:09 +0800

    修改按环境拉配置

--- web/src/config/index.js
+++ web/src/config/index.js
@@ -1,3 +1,3 @@
-export const converterAdress = '0x5EB5E7AE8300Cfc1f08ed6fa5eAe93B95160d996'
-export const tokenAdress = '0xB5de6e68ce4Da88502Be00addd6F33Bc1B5A255b'
-// export const testAdress = '0xb8958D5A4089793c751901034693eDc3708f0324'
+// console.log(process.env)
+export const converterAdress = process.env.VUE_APP_CONVERTERADRESS
+export const tokenAdress = process.env.VUE_APP_TOKENADRESS
