4583c7f0cb4b0f623c523c4db3abf69aae59c63c
guoxbin <guoxbin@gmail.com>
2020-07-28 18:38:11 +0800

    Update public path

--- web/vue.config.js
+++ web/vue.config.js
@@ -2,7 +2,10 @@ const path = require("path");
 
 module.exports = {
 
+    publicPath: process.env.VUE_APP_PUBLIC_PATH ? process.env.VUE_APP_PUBLIC_PATH: '/',
+
     configureWebpack: () => {
+        console.log(": " + process.env.NODE_ENV)
         return {
             output : {
                 filename: 'js/[name].[hash].js'
